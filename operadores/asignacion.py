45af6a676f050e42be0355b762d0b8298550b6d6
Atinare <atinare.dev@gmail.com>
2025-03-17 08:35:20 +0100

    operadores de asignacion basico

---
+++ operadores/asignacion.py
@@ -0,0 +1,8 @@
+# Asignamos edad el valor 5
+edad = 5
+# Esto es lo mismo que decir edad = edad + 5 o menos 5 segun el caso
+edad += 5
+edad -= 5
+# Operador de incremento/decremento: No se puede hacer ++ ni -- hay que hacerlo asi
+edad += 1
+edad -= 1
